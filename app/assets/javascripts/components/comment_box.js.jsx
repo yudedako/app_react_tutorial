43c57701a398b644a7b1394d16435dca8ec30707
yudedako <pluto.club+git@gmail.com>
2016-07-27 17:42:34 +0900

    Hook up the data model

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -3,7 +3,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <br />
         <CommentForm />
       </div>
@@ -13,10 +13,17 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
